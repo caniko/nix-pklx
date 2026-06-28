db0f285dd117e6443f2ae2d2c57bd544d8a264b7
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-06-28 10:25:11 +0200

    chore: add Nix flake with rs-harbor dev shells and crane checks

---
+++ flake.nix
@@ -0,0 +1,109 @@
+{
+  description = "nix-pklx — Pkl ↔ Nix interop: evaluate Pkl files to native Nix expressions + toPkl/fromPkl Nix library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rs-harbor = {
+      url = "git+https://codeberg.org/caniko/rs-harbor.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.crane.follows = "crane";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      rust-overlay,
+      rs-harbor,
+      ...
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+    in
+    {
+      lib = import ./lib { inherit (nixpkgs) lib; };
+
+      packages = forSystems (system:
+        let
+          pkgs = pkgsFor system;
+          craneLib = crane.mkLib pkgs;
+        in {
+          default = craneLib.buildPackage {
+            pname = "pklx";
+            version = "0.1.0";
+            src = craneLib.cleanCargoSource ./.;
+            strictDeps = true;
+            doCheck = true;
+          };
+          pklx = self.packages.${system}.default;
+        }
+      );
+
+      checks = forSystems (system:
+        let
+          pkgs = pkgsFor system;
+          craneLib = crane.mkLib pkgs;
+          src = craneLib.cleanCargoSource ./.;
+          commonArgs = {
+            inherit src;
+            pname = "pklx";
+            strictDeps = true;
+          };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        in
+        {
+          pklx-tests = craneLib.cargoTest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoExtraArgs = "--all-features";
+            }
+          );
+
+          pklx-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoExtraArgs = "--all-features";
+              cargoClippyExtraArgs = "--all-targets -- -D warnings";
+            }
+          );
+
+          pklx-fmt = craneLib.cargoFmt { inherit src; pname = "pklx"; };
+        }
+      );
+
+      devShells = forSystems (system:
+        let
+          pkgs = pkgsFor system;
+          toolchain = rs-harbor.lib.mkToolchain { inherit pkgs; };
+          cargoConfig = rs-harbor.lib.mkCargoConfig { inherit pkgs; };
+          cross = rs-harbor.lib.mkCross { inherit pkgs system; };
+        in
+        rs-harbor.lib.mkDevShells {
+          inherit pkgs cross cargoConfig;
+          inherit (toolchain) craneLib;
+        }
+      );
+    };
+}
